39e6e37006c4b634be9345dab04e442906634083
DenProstoDen <vuda.vvv@mail.ru>
2024-03-06 12:18:35 +0400

    fix background and something else

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,13 +2,11 @@ import AboutProject from "./AboutProject/AboutProject";
 import AboutTechs from "./Tech/Tech";
 import AboutMe from "./AboutMe/AboutMe";
 import PromoProject from "./Promo/Promo";
-import ScrollToUp from "../ScrollUp/ScrollUp";
 
 
 export default function Main() {
     return (
         <main>
-            <ScrollToUp />
             <PromoProject/>
             <AboutProject />
             <AboutTechs />
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -11,55 +11,55 @@ import movieNumber9 from "../images/movieNumber9.jpg"
 const constantFilm = [
     {
         id: 1,
-        name: 'film number 1',
+        name: 'film 1',
         image: movieNumber1,
         duration: 77
     },
     {
         id: 2,
-        name: 'film number 2',
+        name: 'film 2',
         image: movieNumber2,
         duration: 58
     },
     {
         id: 3,
-        name: 'film number 3',
+        name: 'film 3',
         image: movieNumber3,
         duration: 68
     },
     {
         id: 4,
-        name: 'film number 4',
+        name: 'film 4',
         image: movieNumber4,
         duration: 30
     },
     {
         id: 5,
-        name: 'film number 5',
+        name: 'film 5',
         image: movieNumber5,
         duration: 125
     },
     {
         id: 6,
-        name: 'film number 6',
+        name: 'film 6',
         image: movieNumber6,
         duration: 111
     },
     {
         id: 7,
-        name: 'film number 7',
+        name: 'film 7',
         image: movieNumber7,
         duration: 130
     },
     {
         id: 8,
-        name: 'film number 8',
+        name: 'film 8',
         image: movieNumber8,
         duration: 51
     },
     {
         id: 9,
-        name: 'film number 9',
+        name: 'film 9',
         image: movieNumber9,
         duration: 40
     },
